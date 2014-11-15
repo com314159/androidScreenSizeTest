ef6382b5352049b8ce2d4ebce70ce4a2d18abb31
com314159 <com314159@qq.com>
2014-11-15 10:27:07 +0800

    修改一下喽

--- src/com/example/androidscreensize/MainActivity.java
+++ src/com/example/androidscreensize/MainActivity.java
@@ -5,37 +5,40 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
        
        Button button = (Button)findViewById(R.id.id);
+       mTextView = (TextView)findViewById(R.id.textview);
        button.setOnClickListener(new View.OnClickListener() {
-		
+	
+    	  
 		@Override
 		public void onClick(View v) {
 	        DisplayMetrics displayMetrics = new DisplayMetrics();
 	        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 	       int displayWidth = displayMetrics.widthPixels;
 	       int displayHeight = displayMetrics.heightPixels;
-	       String text = "width px = " + String.valueOf(displayWidth);
-	       text = text + " Height px = " + String.valueOf(displayHeight);
-	       text = text + " densith(px/dp) = " + String.valueOf(displayMetrics.density);
-	       text = text + " densithDpi = " + String.valueOf(displayMetrics.densityDpi);
+	       String text = " width px = " + String.valueOf(displayWidth);
+	       text = text + "\n Height px = " + String.valueOf(displayHeight);
+	       text = text + "\n densith(px/dp) = " + String.valueOf(displayMetrics.density);
+	       text = text + "\n densithDpi = " + String.valueOf(displayMetrics.densityDpi);
 	       float widthDPi = displayWidth * 160 /displayMetrics.densityDpi;
 	       float heightDPi = displayHeight * 160 /displayMetrics.densityDpi;
-	       text = text + " totalDpWith = " + String.valueOf(widthDPi);
-	       text = text + " totalDpHeight = " + String.valueOf(heightDPi);
-	       text = text + " values  = " + getResources().getString(R.string.values_res);
+	       text = text + "\n totalDpWith = " + String.valueOf(widthDPi);
+	       text = text + "\n totalDpHeight = " + String.valueOf(heightDPi);
+	       text = text + "\n values  = " + getResources().getString(R.string.values_res);
 	       
 	       
 	       
-	       Toast.makeText(MainActivity.this, text,Toast.LENGTH_LONG).show();
+	      mTextView.setText(text); 
 		}
 	});
     }
